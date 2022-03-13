59ac7127768a060f9e0eb04a26eaf7ae15ed2b5d
Antônio Vinicius <avmoura.r@gmail.com>
2022-03-13 01:21:30 -0300

    finalizing the implementation of the function that finds the encryption key :zap:

--- parte_02/freq_analysis.py
+++ parte_02/freq_analysis.py
@@ -1,74 +1,65 @@
-# Frequency Finder
-# https://www.nostarch.com/crackingcodes (BSD Licensed)
+def contagem_letras(msg_decifrada):
+    alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # Returns a dictionary with keys of single ALFABETO and values of the count of how many times they appear in the message parameter:
+    cont_letras = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
 
-ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for letras in msg_decifrada.upper():
+        if letras in alfabeto:
+            cont_letras[letras] += 1
 
-def getLetterCount(message):
-    # Returns a dictionary with keys of single letters and values of the
-    # count of how many times they appear in the message parameter:
-    letterCount = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+    return cont_letras
 
-    for letter in message.upper():
-        if letter in LETTERS:
-            letterCount[letter] += 1
-
-    return letterCount
-
-def getItemAtIndexZero(items):
+def get_item_0(items):
     return items[0]
 
-def getFrequencyOrder(message):
-    # Returns a string of the alphabet letters arranged in order of most
-    # frequently occurring in the message parameter.
+def get_ordem_freq(msg_decifrada, freq_por_lingua):
+    alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # Returns a string of the alphabet ALFABETO arranged in order of most frequently occurring in the message parameter.
 
     # First, get a dictionary of each letter and its frequency count:
-    letterToFreq = getLetterCount(message)
+    freq_letras = contagem_letras(msg_decifrada)
 
     # Second, make a dictionary of each frequency count to each letter(s)
     # with that frequency:
-    freqToLetter = {}
-    for letter in LETTERS:
-        if letterToFreq[letter] not in freqToLetter:
-            freqToLetter[letterToFreq[letter]] = [letter]
+    freq_por_letra = {}
+    for letra in alfabeto:
+        if freq_letras[letra] not in freq_por_letra:
+            freq_por_letra[freq_letras[letra]] = [letra]
         else:
-            freqToLetter[letterToFreq[letter]].append(letter)
+            freq_por_letra[freq_letras[letra]].append(letra)
 
-    # Third, put each list of letters in reverse "ETAOIN" order, and then
+    # Third, put each list of ALFABETO in reverse "ETAOIN" order, and then
     # convert it to a string:
-    for freq in freqToLetter:
-        freqToLetter[freq].sort(key=ETAOIN.find, reverse=True)
-        freqToLetter[freq] = ''.join(freqToLetter[freq])
+    for freq in freq_por_letra:
+        freq_por_letra[freq].sort(key=freq_por_lingua.find, reverse=True)
+        freq_por_letra[freq] = ''.join(freq_por_letra[freq])
 
     # Fourth, convert the freqToLetter dictionary to a list of
     # tuple pairs (key, value), then sort them:
-    freqPairs = list(freqToLetter.items())
-    freqPairs.sort(key=getItemAtIndexZero, reverse=True)
+    pares_freq = list(freq_por_letra.items())
+    pares_freq.sort(key=get_item_0, reverse=True)
 
-    # Fifth, now that the letters are ordered by frequency, extract all
-    # the letters for the final string:
-    freqOrder = []
-    for freqPair in freqPairs:
-        freqOrder.append(freqPair[1])
+    # Fifth, now that the ALFABETO are ordered by frequency, extract all
+    # the ALFABETO for the final string:
+    ordem_freq = []
+    for freqPair in pares_freq:
+        ordem_freq.append(freqPair[1])
 
-    return ''.join(freqOrder)
+    return ''.join(ordem_freq)
 
-def englishFreqMatchScore(message):
-    # Return the number of matches that the string in the message
-    # parameter has when its letter frequency is compared to English
-    # letter frequency. A "match" is how many of its six most frequent
-    # and six least frequent letters is among the six most frequent and
-    # six least frequent letters for English.
-    freqOrder = getFrequencyOrder(message)
+def pontuacao_freq(msg_decifrada, idioma_provavel="en"):
 
-    matchScore = 0
-    # Find how many matches for the six most common letters there are:
-    for commonLetter in ETAOIN[:6]:
-        if commonLetter in freqOrder[:6]:
-            matchScore += 1
-    # Find how many matches for the six least common letters there are:
-    for uncommonLetter in ETAOIN[-6:]:
-        if uncommonLetter in freqOrder[-6:]:
-            matchScore += 1
+    freq_por_lingua = 'AEOSRINDMUTCLPVGHQBFZJXKWY' if idioma_provavel == "pt" else 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+    ordem_freq = get_ordem_freq(msg_decifrada, freq_por_lingua)
 
-    return matchScore
+    pontuacao = 0
+    # Find how many matches for the six most common ALFABETO there are:
+    for letras_mais_freq in freq_por_lingua[:6]:
+        if letras_mais_freq in ordem_freq[:6]:
+            pontuacao += 1
+    # Find how many matches for the six least common ALFABETO there are:
+    for letras_menos_freq in freq_por_lingua[-6:]:
+        if letras_menos_freq in ordem_freq[-6:]:
+            pontuacao += 1
+
+    return pontuacao
--- parte_02/vigenere_hacker.py
+++ parte_02/vigenere_hacker.py
@@ -1,242 +1,239 @@
-# Vigenere Cipher Hacker
-# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
-
 import itertools, re
-import vigenere_cipher, freq_analysis, detect_english
+import freq_analysis, detect_language
 
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-SILENT_MODE = False # If set to True, program doesn't print anything.
 NUM_MOST_FREQ_LETTERS = 4 # Attempt this many letters per subkey.
 MAX_KEY_LENGTH = 16 # Will not attempt keys longer than this.
-NONLETTERS_PATTERN = re.compile('[^A-Z]')
 
-def main():
-    with open("parte_02/desafio1.txt") as f:
-        content = f.read()
-
-    msg_cifrada = content
-    msg_decifrada = decifrador_vigenere(msg_cifrada)
-
-    if msg_decifrada != None:
-        print('Copying hacked message to clipboard:')
-        print(msg_decifrada)
-    else:
-        print('Failed to hack encryption.')
-
-def findRepeatSequencesSpacings(message):
+def encontra_distancia_seq(msg_cifrada):
     # Goes through the message and finds any 3 to 5 letter sequences
     # that are repeated. Returns a dict with the keys of the sequence and
     # values of a list of spacings (num of letters between the repeats).
 
     # Use a regular expression to remove non-letters from the message:
-    message = NONLETTERS_PATTERN.sub('', message.upper())
+    filtro = re.compile('[^A-Z]')
+    msg_cifrada = filtro.sub('', msg_cifrada.upper())
 
     # Compile a list of seqLen-letter sequences found in the message:
-    seqSpacings = {} # Keys are sequences, values are lists of int spacings.
-    for seqLen in range(3, 6):
-        for seqStart in range(len(message) - seqLen):
+    distancia_seq = {} # Keys are sequences, values are lists of int spacings.
+    for tamanho_seq in range(3, 6):
+        for inicio_seq in range(len(msg_cifrada) - tamanho_seq):
             # Determine what the sequence is, and store it in seq:
-            seq = message[seqStart:seqStart + seqLen]
+            seq = msg_cifrada[inicio_seq:inicio_seq + tamanho_seq]
 
             # Look for this sequence in the rest of the message:
-            for i in range(seqStart + seqLen, len(message) - seqLen):
-                if message[i:i + seqLen] == seq:
+            for i in range(inicio_seq + tamanho_seq, len(msg_cifrada) - tamanho_seq):
+                if msg_cifrada[i:i + tamanho_seq] == seq:
                     # Found a repeated sequence.
-                    if seq not in seqSpacings:
-                        seqSpacings[seq] = [] # Initialize a blank list.
+                    if seq not in distancia_seq:
+                        distancia_seq[seq] = [] # Initialize a blank list.
 
                     # Append the spacing distance between the repeated
                     # sequence and the original sequence:
-                    seqSpacings[seq].append(i - seqStart)
-    return seqSpacings
+                    distancia_seq[seq].append(i - inicio_seq)
+    return distancia_seq
 
-def getUsefulFactors(num):
+def multiplos_uteis(numero):
     # Returns a list of useful factors of num. By "useful" we mean factors
     # less than MAX_KEY_LENGTH + 1 and not 1. For example,
-    # getUsefulFactors(144) returns [2, 3, 4, 6, 8, 9, 12, 16]
+    # multiplos_uteis(144) returns [2, 3, 4, 6, 8, 9, 12, 16]
 
-    if num < 2:
+    if numero < 2:
         return [] # Numbers less than 2 have no useful factors.
 
-    factors = [] # The list of factors found.
+    multiplos = [] # The list of factors found.
 
     # When finding factors, you only need to check the integers up to
     # MAX_KEY_LENGTH.
     for i in range(2, MAX_KEY_LENGTH + 1): # Don't test 1: it's not useful.
-        if num % i == 0:
-            factors.append(i)
-            otherFactor = int(num / i)
-            if otherFactor < MAX_KEY_LENGTH + 1 and otherFactor != 1:
-                factors.append(otherFactor)
-    return list(set(factors)) # Remove duplicate factors.
+        if numero % i == 0: #se for par
+            multiplos.append(i)
+            outro_multiplo = int(numero / i)
+            if (outro_multiplo < MAX_KEY_LENGTH + 1) and (outro_multiplo != 1):
+                multiplos.append(outro_multiplo)
+    return list(set(multiplos)) # Remove duplicate factors.
 
-def getItemAtIndexOne(items):
+def get_item_1(items):
     return items[1]
 
-def getMostCommonFactors(seqFactors):
+def conta_multiplos_frequentes(seq_multiplo):
     # First, get a count of how many times a factor occurs in seqFactors:
-    factorCounts = {} # Key is a factor, value is how often it occurs.
+    contagem_multiplo = {} # Key is a factor, value is how often it occurs.
 
     # seqFactors keys are sequences, values are lists of factors of the
     # spacings. seqFactors has a value like: {'GFD': [2, 3, 4, 6, 9, 12,
     # 18, 23, 36, 46, 69, 92, 138, 207], 'ALW': [2, 3, 4, 6, ...], ...}
-    for seq in seqFactors:
-        factorList = seqFactors[seq]
-        for factor in factorList:
-            if factor not in factorCounts:
-                factorCounts[factor] = 0
-            factorCounts[factor] += 1
+    for seq in seq_multiplo:
+        lista_multiplo = seq_multiplo[seq]
+        for multiplo in lista_multiplo:
+            if multiplo not in contagem_multiplo:
+                contagem_multiplo[multiplo] = 0
+            contagem_multiplo[multiplo] += 1
 
     # Second, put the factor and its count into a tuple, and make a list
     # of these tuples so we can sort them:
-    factorsByCount = []
-    for factor in factorCounts:
+    contagem_por_multiplo = []
+    for multiplo in contagem_multiplo:
         # Exclude factors larger than MAX_KEY_LENGTH:
-        if factor <= MAX_KEY_LENGTH:
+        if multiplo <= MAX_KEY_LENGTH:
             # factorsByCount is a list of tuples: (factor, factorCount)
             # factorsByCount has a value like: [(3, 497), (2, 487), ...]
-            factorsByCount.append( (factor, factorCounts[factor]) )
+            contagem_por_multiplo.append( (multiplo, contagem_multiplo[multiplo]) )
 
     # Sort the list by the factor count:
-    factorsByCount.sort(key=getItemAtIndexOne, reverse=True)
+    contagem_por_multiplo.sort(key=get_item_1, reverse=True)
 
-    return factorsByCount
+    return contagem_por_multiplo
 
-def kasiskiExamination(msg_cifrada):
+def analise_kasiski(msg_cifrada):
     # Find out the sequences of 3 to 5 letters that occur multiple times
     # in the msg_cifrada. repeatedSeqSpacings has a value like:
     # {'EXG': [192], 'NAF': [339, 972, 633], ... }
-    repeatedSeqSpacings = findRepeatSequencesSpacings(msg_cifrada)
+    distancia_seq = encontra_distancia_seq(msg_cifrada)
 
-    # (See getMostCommonFactors() for a description of seqFactors.)
-    seqFactors = {}
-    for seq in repeatedSeqSpacings:
-        seqFactors[seq] = []
-        for spacing in repeatedSeqSpacings[seq]:
-            seqFactors[seq].extend(getUsefulFactors(spacing))
+    # (See conta_multiplos_frequentes() for a description of seqFactors.)
+    seq_multiplo = {}
+    for seq in distancia_seq:
+        seq_multiplo[seq] = []
+        for dist in distancia_seq[seq]:
+            seq_multiplo[seq].extend(multiplos_uteis(dist))
 
-    # (See getMostCommonFactors() for a description of factorsByCount.)
-    factorsByCount = getMostCommonFactors(seqFactors)
+    # (See conta_multiplos_frequentes() for a description of factorsByCount.)
+    contagem_por_multiplo = conta_multiplos_frequentes(seq_multiplo)
 
     # Now we extract the factor counts from factorsByCount and
     # put them in lista_de_comprimentos so that they are easier to
     # use later:
-    lista_de_comprimentos = []
-    for twoIntTuple in factorsByCount:
-        lista_de_comprimentos.append(twoIntTuple[0])
+    comprimentos_possiveis = []
+    for tupla in contagem_por_multiplo:
+        comprimentos_possiveis.append(tupla[0])
 
-    return lista_de_comprimentos
+    return comprimentos_possiveis
 
-def getNthSubkeysLetters(repeticao, comprimento_chave, msg_cifrada):
+def letras_subchave_n(sub_chave, comprimento_chave, msg_cifrada):
     # Returns every nth letter for each keyLength set of letters in text.
-    # E.g. getNthSubkeysLetters(1, 3, 'ABCABCABC') returns 'AAA'
-    #      getNthSubkeysLetters(2, 3, 'ABCABCABC') returns 'BBB'
-    #      getNthSubkeysLetters(3, 3, 'ABCABCABC') returns 'CCC'
-    #      getNthSubkeysLetters(1, 5, 'ABCDEFGHI') returns 'AF'
+    # E.g. letras_subchave_n(1, 3, 'ABCABCABC') returns 'AAA'
+    #      letras_subchave_n(2, 3, 'ABCABCABC') returns 'BBB'
+    #      letras_subchave_n(3, 3, 'ABCABCABC') returns 'CCC'
+    #      letras_subchave_n(1, 5, 'ABCDEFGHI') returns 'AF'
 
     # Use a regular expression to remove non-letters from the message:
-    msg_cifrada = NONLETTERS_PATTERN.sub('', msg_cifrada)
+    filtro = re.compile('[^A-Z]')
+    msg_cifrada = filtro.sub('', msg_cifrada)
 
-    i = repeticao - 1
+    i = sub_chave - 1
     letras = []
     while i < len(msg_cifrada):
         letras.append(msg_cifrada[i])
         i += comprimento_chave
     return ''.join(letras)
 
-def attemptHackWithKeyLength(msg_cifrada, comprimento_chave_provavel):
+def quebra_cifra(msg_cifrada, comprimento, idioma_provavel):
+    alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     # Determine the most likely letters for each letter in the key:
     msg_cifrada_maiuscula = msg_cifrada.upper()
     # allFreqScores is a list of mostLikelyKeyLength number of lists.
     # These inner lists are the freqScores lists.
-    pontuacoes_freq = []
-    for repeticao in range(1, comprimento_chave_provavel + 1):
-        letras_repetidas = getNthSubkeysLetters(repeticao, comprimento_chave_provavel, msg_cifrada_maiuscula)
+    lista_pontuacao_freq = []
+    for sub_chave in range(1, comprimento + 1):
+        letras = letras_subchave_n(sub_chave, comprimento, msg_cifrada_maiuscula)
 
-        # freqScores is a list of tuples like:
-        # [(<letter>, <Eng. Freq. match score>), ... ]
-        # List is sorted by match score. Higher score means better match.
-        # See the englishFreqMatchScore() comments in freq_analysis.py.
         pontuacao_freq = []
 
-        for possivel_chave in LETTERS:
-            msg_decifrada = vigenere_cipher.decryptMessage(possivel_chave, letras_repetidas)
-            qtd_possivel_chave = (possivel_chave, freq_analysis.englishFreqMatchScore(msg_decifrada))
+        for possivel_subchave in alfabeto:
+            msg_decifrada = decifrar_mensagem(possivel_subchave, letras, alfabeto)
+            qtd_possivel_chave = (possivel_subchave, freq_analysis.pontuacao_freq(msg_decifrada, idioma_provavel))
             pontuacao_freq.append(qtd_possivel_chave)
-        # Sort by match score:
-        pontuacao_freq.sort(key=getItemAtIndexOne, reverse=True)
 
-        pontuacoes_freq.append(pontuacao_freq[:NUM_MOST_FREQ_LETTERS])
+        pontuacao_freq.sort(key=get_item_1, reverse=True)
 
-    for i in range(len(pontuacoes_freq)):
-        # Use i + 1 so the first letter is not called the "0th" letter:
-        print('Possible letters for letter %s of the key: ' % (i + 1), end='')
-        for freqScore in pontuacoes_freq[i]:
-            print('%s ' % freqScore[0], end='')
-        print() # Print a newline.
+        lista_pontuacao_freq.append(pontuacao_freq[:NUM_MOST_FREQ_LETTERS])
 
     # Try every combination of the most likely letters for each position
     # in the key:
-    for indexes in itertools.product(range(NUM_MOST_FREQ_LETTERS), repeat=comprimento_chave_provavel):
+    for indices in itertools.product(range(NUM_MOST_FREQ_LETTERS), repeat=comprimento):
+
         # Create a possible key from the letters in allFreqScores:
-        possibleKey = ''
-        for i in range(comprimento_chave_provavel):
-            possibleKey += pontuacoes_freq[i][indexes[i]][0]
+        possivel_chave = ''
+        for i in range(comprimento):
+            possivel_chave += lista_pontuacao_freq[i][indices[i]][0]
 
-        if not SILENT_MODE:
-            print('Attempting with key: %s' % (possibleKey))
+        print('Possível chave: %s' % (possivel_chave))
 
-        decryptedText = vigenere_cipher.decryptMessage(possibleKey, msg_cifrada_maiuscula)
+        texto_decifrado = decifrar_mensagem(possivel_chave, msg_cifrada_maiuscula, alfabeto)
 
-        if detect_english.isEnglish(decryptedText):
-            # Set the hacked msg_cifrada to the original casing:
-            origCase = []
-            for i in range(len(msg_cifrada)):
-                if msg_cifrada[i].isupper():
-                    origCase.append(decryptedText[i].upper())
-                else:
-                    origCase.append(decryptedText[i].lower())
-            decryptedText = ''.join(origCase)
+        if detect_language.legivel(texto_decifrado, idioma_provavel):
 
             # Check with user to see if the key has been found:
-            print('Possible encryption hack with key %s:' % (possibleKey))
-            print(decryptedText[:200]) # Only show first 200 characters.
-            print('\nEnter D if done, anything else to continue hacking:')
+            print(f'\nTexto decifrado com a chave "{possivel_chave}":\n')
+            print(texto_decifrado)
+
+            print(f'\nEstá correto? Aperte S para sim e N para não [S/N]')
             response = input('> ')
 
-            if response.strip().upper().startswith('D'):
-                return decryptedText
+            if response.strip().upper().startswith('S'):
+                return texto_decifrado
 
     # No English-looking decryption found, so return None:
     return None
 
-def decifrador_vigenere(msg_cifrada):
-    lista_de_comprimentos = kasiskiExamination(msg_cifrada) # Retorna a lista de comprimentos possiveis que a chave pode ter
+def decifrador_vigenere(msg_cifrada, idioma_provavel): 
+    lista_de_comprimentos = analise_kasiski(msg_cifrada) # Retorna a lista de comprimentos possiveis que a chave pode ter
 
-    print("Kasiski Examination results say the most likely key lengths are: {lista_de_comprimentos}\n")
+    print(f"\nComprimentos possíveis da chave: {lista_de_comprimentos}")
     
     msg_decifrada = None
 
     for comprimento in lista_de_comprimentos:
-        print(f"Attempting hack with key length {comprimento} ({NUM_MOST_FREQ_LETTERS ** comprimento} possible keys)...")
+        print(f"Tentando hackear com comprimento de chave {comprimento}\n")
 
-        msg_decifrada = attemptHackWithKeyLength(msg_cifrada, comprimento)
+        msg_decifrada = quebra_cifra(msg_cifrada, comprimento, idioma_provavel)
+
         if msg_decifrada != None:
             break
 
     # If none of the key lengths we found using Kasiski Examination
     # worked, start brute-forcing through key lengths:
-    if msg_decifrada == None:
-        print('Unable to hack message with likely key length(s). Brute forcing key length...')
-        for comprimento_chave in range(1, MAX_KEY_LENGTH + 1):
-            # Don't re-check key lengths already tried from Kasiski:
-            if comprimento_chave not in lista_de_comprimentos:
-                print(f"Attempting hack with key length {comprimento_chave} ({NUM_MOST_FREQ_LETTERS ** comprimento_chave} possible keys)...")
-                msg_decifrada = attemptHackWithKeyLength(msg_cifrada, comprimento_chave)
-                if msg_decifrada != None:
-                    break
+    # if msg_decifrada == None:
+    #     print('Unable to hack message with likely key length(s). Brute forcing key length...')
+    #     for comprimento_chave in range(1, MAX_KEY_LENGTH + 1):
+    #         # Don't re-check key lengths already tried from Kasiski:
+    #         if comprimento_chave not in lista_de_comprimentos:
+    #             print(f"Attempting hack with key length {comprimento_chave} ({NUM_MOST_FREQ_LETTERS ** comprimento_chave} possible keys)...")
+    #             msg_decifrada = quebra_cifra(msg_cifrada, comprimento_chave)
+    #             if msg_decifrada != None:
+    #                 break
+
+
     return msg_decifrada
 
+def decifrar_mensagem(chave, mensagem, alfabeto):
+    palavra_cifrada = []  # Definição do vetor com palavra cifrada
+
+    indice_chave = 0  # Armazenamento do valor atual do índice da chave
+
+    for caractere in mensagem:
+        if caractere in alfabeto:  # Verificar se o caracter está no alfabeto para ser cifrado
+            num_caractere_cifrado = (alfabeto.index(caractere) - alfabeto.index(chave[indice_chave])) % len(alfabeto)  # Se estiver, busca o indice e soma ao indice correspondente da chave módulo tamanho do alfabeto
+            caractere_cifrado = alfabeto[num_caractere_cifrado]  # Transforma indice em caratere cifrado
+            palavra_cifrada.append(caractere_cifrado)  # Insere caractere no vetor de palavra cifrada
+            indice_chave = (indice_chave + 1) % len(chave)  # Atualiza o indice atual da chave
+        else:
+            palavra_cifrada.append(caractere)
+
+    return ''.join(palavra_cifrada)
+
+def main():
+
+    msg_cifrada = input('Digite a mensagem: ')
+    idioma_provavel = input('Digite o provável idioma: ')
+
+    with open("parte_02/desafio2.txt") as f:
+        msg_cifrada = f.read()
+
+    msg_decifrada = decifrador_vigenere(msg_cifrada, idioma_provavel)
+
+    if msg_decifrada == None:
+        print('Failed to hack encryption.')
 
 # If vigenereHacker.py is run (instead of imported as a module) call
 # the main() function.
